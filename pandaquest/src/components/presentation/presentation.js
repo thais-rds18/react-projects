8d0c08bfe625547ed51e1ef172f2040302ff01eb
Thaís Tawany <99848191+thais-rds18@users.noreply.github.com>
2022-11-27 23:27:43 -0300

    alt posterior

--- pandaquest/src/components/presentation/presentation.js
+++ pandaquest/src/components/presentation/presentation.js
@@ -28,8 +28,6 @@ export default function Presentation() {
                 <Text fontSize='3xl'>Afinal, é um clássico magnifíco!!</Text>
                 <Text fontSize='3xl'>Que tal participar de um pequeno questionário sobre a saga?</Text>
                 <br />
-                <Text fontSize='3xl'> pessoas já responderam!</Text>
-                <br />
                 <Text fontSize='3xl'>Vamos começar!! Digite seu nome!</Text>
                 <br />
                 <Input id="nome" placeholder='Seu nome aqui' value={nome} onChange={(novonome) => { setNome(novonome.target.value); }} size='lg' width={500} />
